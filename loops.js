df927b9971d57ea975663ad12ca5e84291677aec
Eric Aniag <ericaniag@gmail.com>
2019-04-25 21:43:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,8 +17,8 @@ function whileLoop(n) {
   return 'done'
 }
 
-function doWhileLoop(num) {
-  do {
-    console.log("I run once regardless.")
-  } while (incrementVariable() < num)
+var i = 0
+function incrementVariable() {
+  i = i + 1 
+  return i 
 }
